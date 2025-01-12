f3864677535d3b26ee6541fea462664d6d298a0f
Allan <workofcode@gmail.com>
2025-01-12 13:26:39 +0000

    - Fix, empty inputArray did not generate pills

--- src/main.ts
+++ src/main.ts
@@ -5,45 +5,45 @@ function createPillItem(
   item: { brandName: string; mdiIcon: string | null; humanReadable: string },
   options: { rounded?: boolean; spacing?: 'small' | 'medium' | 'large' } = {}
 ) {
-  const { rounded = false, spacing = 'small' } = options;
+  const { rounded = false, spacing = 'small' } = options
 
-  const element = document.createElement('div');
+  const element = document.createElement('div')
   let iconElement;
-  element.className = `brandpill ${item.brandName}`;
+  element.className = `brandpill ${item.brandName}`
 
   // If there's an icon
   if (item.mdiIcon) {
-    element.classList.add('icon');
+    element.classList.add('icon')
     iconElement = document.createElement('i');
-    iconElement.classList.add('mdi', item.mdiIcon);
-    element.prepend(iconElement);
+    iconElement.classList.add('mdi', item.mdiIcon)
+    element.prepend(iconElement)
   }
 
   if (rounded) {
-    element.classList.add('rounded');
+    element.classList.add('rounded')
   }
 
   if (['small', 'medium', 'large'].includes(spacing)) {
-    element.classList.add('spaced-' + spacing);
+    element.classList.add('spaced-' + spacing)
   }
 
-  element.append(item.humanReadable);
-  return element;
+  element.append(item.humanReadable)
+  return element
 }
 
 export function createPills(
   brandListInput: string[] | null = null,
   options: { rounded?: boolean; spacing?: 'small' | 'medium' | 'large' } = {}
 ) {
-  const { rounded = false, spacing = 'small' } = options;
+  const { rounded = false, spacing = 'small' } = options
 
   // Use a map for quicker lookup.
-  const brandsMap = new Map();
-  brands.forEach((brand) => brandsMap.set(brand.brandName, brand));
+  const brandsMap = new Map()
+  brands.forEach((brand) => brandsMap.set(brand.brandName, brand))
 
   // Container for our pills
-  const container = document.createElement('div');
-  container.classList.add('brandpill-container');
+  const container = document.createElement('div')
+  container.classList.add('brandpill-container')
 
   const pills: Array<Node> = [];
 
@@ -51,16 +51,24 @@ export function createPills(
     ? brandListInput.map((name) => brandsMap.get(name))
     : brands;
 
-  matchingBrands.forEach((item) => {
-    if (item) {
-      const pill = createPillItem(item, { rounded, spacing });
-      pills.push(pill);
-    }
-  });
+  if(!brandListInput || brandListInput.length === 0){
+    brands.forEach(item => {
+      const pill = createPillItem(item, { rounded, spacing })
+      pills.push(pill)      
+    })
+  } else {
+    matchingBrands.forEach((item) => {
+      if (item) {
+        const pill = createPillItem(item, { rounded, spacing })
+        pills.push(pill);
+      }
+    })
+  }
+
 
   pills.forEach((pill) => {
-    container.appendChild(pill);
+    container.appendChild(pill)
   });
 
-  return container;
+  return container
 }
