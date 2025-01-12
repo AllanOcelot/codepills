6d9f1f4e437f9043adf3b77041259a04170e12ae
Allan <workofcode@gmail.com>
2025-01-12 16:05:44 +0000

    - add, ability to toggle icons on or off

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './styles/main.scss'
 
 export function getBrands(){
   const listContainer = document.createElement('ul')
-
   brands.forEach(element => {
     let itemNode = document.createElement('li')
     itemNode.innerHTML = element.humanReadable
@@ -20,14 +19,20 @@ function createPillItem(
     iconsEnabled?: boolean;
   } = {}
 ) {
-  const { rounded = false, spacing = 'small' } = options
+
+  // Deconstruct the options, set default values on items
+  const { 
+    rounded = false, 
+    spacing = 'small', 
+    iconsEnabled = true
+  } = options
 
   const element = document.createElement('div')
   let iconElement;
   element.className = `brandpill ${item.brandName}`
 
   // If there's an icon
-  if (item.mdiIcon && item.mdiIcon.trim() !== '') {
+  if (iconsEnabled && item.mdiIcon && item.mdiIcon.trim() !== '') {
     element.classList.add('icon')
     iconElement = document.createElement('i');
     iconElement.classList.add('mdi', item.mdiIcon)
@@ -46,26 +51,45 @@ function createPillItem(
   return element
 }
 
+
+
 export function createPills(
   brandListInput: string[] | null = null,
   options: { 
     rounded?: boolean; 
     spacing?: 'small' | 'medium' | 'large' 
     align?: 'left' | 'center' | 'centre' | 'right'
+    iconsEnabled?: boolean
   } = {}
 ) {
-  const { rounded = false, spacing = 'small', align = 'left' } = options
 
-  // Use a map for quicker lookup.
-  const brandsMap = new Map()
-  brands.forEach((brand) => brandsMap.set(brand.brandName, brand))
+  // deconstruct from options
+  const { 
+    rounded = false, 
+    spacing = 'small', 
+    align = 'left',
+    iconsEnabled = true
+  } = options
 
-  // valid inputs for aligment are
+  // HTML node elements we inject into each other or DOM
+  const pills: Array<Node> = [];
+  const container = document.createElement('div')
+
+
+  // valid inputs for alignment are
   const valuesAlign = new Set(["left", "center", "centre", "right"])
 
 
+  // map our brands for quicker lookup
+  const brandsMap = new Map()
+  brands.forEach((brand) => brandsMap.set(brand.brandName, brand))
+  // the results of our search on user provided input
+  const matchingBrands = brandListInput
+  ? brandListInput.map((name) => brandsMap.get(name))
+  : brands;
+
+
   // Container for our pills
-  const container = document.createElement('div')
   container.classList.add('brandpill-container')
   if(valuesAlign.has(align)){
     container.classList.add(align)
@@ -73,21 +97,17 @@ export function createPills(
     console.warn('CodePills: None Valid value entered for alignment, not applied.')
   }
 
-  const pills: Array<Node> = [];
 
-  const matchingBrands = brandListInput
-    ? brandListInput.map((name) => brandsMap.get(name))
-    : brands;
-
+  // If user does not provide
   if(!brandListInput || brandListInput.length === 0){
     brands.forEach(item => {
-      const pill = createPillItem(item, { rounded, spacing })
+      const pill = createPillItem(item, { rounded, spacing, iconsEnabled })
       pills.push(pill)      
     })
   } else {
     matchingBrands.forEach((item) => {
       if (item) {
-        const pill = createPillItem(item, { rounded, spacing })
+        const pill = createPillItem(item, { rounded, spacing, iconsEnabled })
         pills.push(pill);
       }
     })
