c6622c5174157b055fb2e1670a5001692a4e0118
Allan <workofcode@gmail.com>
2025-01-12 13:12:30 +0000

    overhaul index.html with better examples, refactor main.ts to accept optional arguments

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,66 @@
 import { brands } from './brands'
 import './styles/main.scss'
 
-function createPillItem(item: {brandName: string, mdiIcon: string | null, humanReadable: string}, rounded: Boolean | null = false, spacing: string | null = 'small'){
-  const element = document.createElement('div')
+function createPillItem(
+  item: { brandName: string; mdiIcon: string | null; humanReadable: string },
+  options: { rounded?: boolean; spacing?: 'small' | 'medium' | 'large' } = {}
+) {
+  const { rounded = false, spacing = 'small' } = options;
+
+  const element = document.createElement('div');
   let iconElement;
-  element.className = `brandpill ${item.brandName}`
+  element.className = `brandpill ${item.brandName}`;
 
   // If there's an icon
-  if(item.mdiIcon){
-    element.classList.add('icon')
-    iconElement = document.createElement('i')
-    iconElement.classList.add('mdi', item.mdiIcon)
-    element.prepend(iconElement)
+  if (item.mdiIcon) {
+    element.classList.add('icon');
+    iconElement = document.createElement('i');
+    iconElement.classList.add('mdi', item.mdiIcon);
+    element.prepend(iconElement);
   }
 
-  if(rounded){
-    element.classList.add('rounded')
+  if (rounded) {
+    element.classList.add('rounded');
   }
 
-
-  if(spacing === 'small' || spacing === 'medium' || spacing === 'large'){
-      element.classList.add('spaced-' + spacing)
+  if (['small', 'medium', 'large'].includes(spacing)) {
+    element.classList.add('spaced-' + spacing);
   }
 
-  element.append(item.humanReadable)
-  return element
+  element.append(item.humanReadable);
+  return element;
 }
 
-export function createPills(brandListInput: [string], rounded: Boolean | null = false, spacing: string | null = 'small'){
-  // Use a map for quicker lookup.
-  const brandsMap = new Map()
-  brands.forEach(brand => brandsMap.set(brand.brandName, brand));
+export function createPills(
+  brandListInput: string[] | null = null,
+  options: { rounded?: boolean; spacing?: 'small' | 'medium' | 'large' } = {}
+) {
+  const { rounded = false, spacing = 'small' } = options;
 
+  // Use a map for quicker lookup.
+  const brandsMap = new Map();
+  brands.forEach((brand) => brandsMap.set(brand.brandName, brand));
 
   // Container for our pills
-  const container = document.createElement('div'); 
-  container.classList.add('brandpill-container'); 
+  const container = document.createElement('div');
+  container.classList.add('brandpill-container');
 
-  const pills: Array<Node> = []
+  const pills: Array<Node> = [];
 
-  if(brandListInput){
-    const matchingBrands = brandListInput.map(name => brandsMap.get(name));
-    matchingBrands.forEach( item => {
-      const pill = createPillItem(item, rounded, spacing)
-      pills.push(pill)
-    })
-  }else{
-    brands.forEach( item => {
-      pills.push(createPillItem(item))
-    })
-  }
+  const matchingBrands = brandListInput
+    ? brandListInput.map((name) => brandsMap.get(name))
+    : brands;
 
-  pills.forEach( pill => {
-    container.appendChild(pill)
-  })
+  matchingBrands.forEach((item) => {
+    if (item) {
+      const pill = createPillItem(item, { rounded, spacing });
+      pills.push(pill);
+    }
+  });
 
-  return container
+  pills.forEach((pill) => {
+    container.appendChild(pill);
+  });
+
+  return container;
 }
